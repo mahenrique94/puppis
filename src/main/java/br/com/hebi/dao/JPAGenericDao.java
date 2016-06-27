424dcb15b00679b3d93e0c04e0504b72a351005b
Matheus <matheushcastiglioni@gmail.com>
2016-06-27 19:59:37 -0300

    Efetuado acerto no JPAGenericDao para sempre instanciar um novo ParametrosWebBuilder

--- src/main/java/br/com/hebi/dao/JPAGenericDao.java
+++ src/main/java/br/com/hebi/dao/JPAGenericDao.java
@@ -13,7 +13,6 @@ public class JPAGenericDao<T> implements GenericDao<T>{
 
 	@Inject
 	private EntityManager em;
-	private final ParametrosWebBuilder parametrosWebBuilder = new ParametrosWebBuilder();
 
 	public JPAGenericDao(EntityManager em) {
 		this.em = em;
@@ -38,7 +37,7 @@ public class JPAGenericDao<T> implements GenericDao<T>{
 	public T find(Class clazz, List<ParametrosWeb> parametrosWeb) {
 		// TODO Auto-generated method stub
 		try {
-			return (T) this.em.createQuery(parametrosWebBuilder.execute(clazz, parametrosWeb)).setFirstResult(0).setMaxResults(1).getSingleResult();
+			return (T) this.em.createQuery(new ParametrosWebBuilder().execute(clazz, parametrosWeb)).setFirstResult(0).setMaxResults(1).getSingleResult();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -50,7 +49,7 @@ public class JPAGenericDao<T> implements GenericDao<T>{
 	public List<T> findLimit(Class clazz, List<ParametrosWeb> parametrosWeb, Integer limite) {
 		// TODO Auto-generated method stub
 		try {
-			return this.em.createQuery(parametrosWebBuilder.execute(clazz, parametrosWeb)).setFirstResult(0).setMaxResults(limite).getResultList();
+			return this.em.createQuery(new ParametrosWebBuilder().execute(clazz, parametrosWeb)).setFirstResult(0).setMaxResults(limite).getResultList();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -63,7 +62,7 @@ public class JPAGenericDao<T> implements GenericDao<T>{
 		// TODO Auto-generated method stub
 		int max = parametrosWeb != null ? parametrosWeb.get(0).getLimit() : 100;
 		try {
-			return this.em.createQuery(parametrosWebBuilder.execute(clazz, parametrosWeb)).setFirstResult(0).setMaxResults(max).getResultList();
+			return this.em.createQuery(new ParametrosWebBuilder().execute(clazz, parametrosWeb)).setFirstResult(0).setMaxResults(max).getResultList();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -74,7 +73,7 @@ public class JPAGenericDao<T> implements GenericDao<T>{
 	@Override
 	public List<T> findPagination(Class clazz, List<ParametrosWeb> parametrosWeb, Paginator paginator) {
 		try {
-			return this.em.createQuery(parametrosWebBuilder.execute(clazz, parametrosWeb)).setFirstResult(paginator.getFirst()).setMaxResults(paginator.getInterval()).getResultList();
+			return this.em.createQuery(new ParametrosWebBuilder().execute(clazz, parametrosWeb)).setFirstResult(paginator.getFirst()).setMaxResults(paginator.getInterval()).getResultList();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
